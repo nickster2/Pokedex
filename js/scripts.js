73e444cd5f20d9b43a329b5053beabdee5e101b1
nickster2 <gouldnick125@gmail.com>
2024-06-11 07:44:10 -0700

    introduced list of Pokemon
    added a variable containing an array of pokemon names, attributes, and heights

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,32 @@
-alert('Hello World');
-
-let favoriteFood = 'Tacos';
-document.write(favoriteFood);
+var pokemonList = [];
+  let pokemonList = [{ 
+    name: 'Bulbasaur',
+    height: 2.04,
+    type: ['grass', 'poison']
+  },
+  {
+    name: 'Ivysaur',
+    height: 3.03,
+    type: ['grass', 'poison']
+  },
+  {
+    name: 'Venusaur',
+    height: 6.07,
+    type: ['grass', 'poison'],
+  },
+  {
+    name: 'Charmander',
+    height: 2.00,
+    type: ['fire'],
+  },
+  {
+    name: 'Charmeleon',
+    height: 3.07,
+    type: ['fire'],
+  },
+  {
+    name: 'Charizard',
+    height: 5.07,
+    type: ['fire', 'flying'],
+  },
+]
