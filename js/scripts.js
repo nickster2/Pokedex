9d7a6d099294af871fafcdb9f2b241e775f72f81
nickster2 <gouldnick125@gmail.com>
2024-07-13 22:21:20 -0700

    updated pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,65 @@
+  pokemonList = [
+    {
+      name: 'Bulbasaur',
+      height: 2.04,
+      type: ['grass', 'poison']
+    },
+    {
+      name: 'Ivysaur',
+      height: 3.03,
+      type: ['grass', 'poison']
+    },
+    {
+      name: 'Venusaur',
+      height: 6.07,
+      type: ['grass', 'poison'],
+    },
+    {
+      name: 'Charmander',
+      height: 2.00,
+      type: ['fire'],
+    },
+    {
+      name: 'Charmeleon',
+      height: 3.07,
+      type: ['fire'],
+    },
+    {
+      name: 'Charizard',
+      height: 5.07,
+      type: ['fire', 'flying'],
+    },
 
+    {
+      name: 'Squirtle',
+      height: 1.08,
+      type: ['water']
+    },
+    {
+      name: 'Wartortle',
+      height: 3.03,
+      type: ['water']
+    },
+    {
+      name: 'Blastoise',
+      height: 5.03,
+      type: ['water'],
+    },
+    {
+      name: 'Caterpie',
+      height: 1.00,
+      type: ['bug'],
+    },
+    {
+      name: 'Metapod',
+      height: 2.04,
+      type: ['bug'],
+    },
+    {
+      name: 'Butterfree',
+      height: 3.07,
+      type: ['bug', 'flying']
+    },
+  ];
 
 
